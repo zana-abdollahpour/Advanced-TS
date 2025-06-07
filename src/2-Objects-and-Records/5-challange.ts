f72f8eaef02bc769a63a0a35cbaec8d0b9c1d9c9
zana-abdollahpour <zana.ap4@gmail.com>
2025-06-07 09:40:12 +0330

    4-5

--- src/2-Objects-and-Records/5-challange.ts
+++ src/2-Objects-and-Records/5-challange.ts
@@ -1,7 +1,7 @@
 import { Equal, Expect } from "..";
 
 namespace assign {
-  type Assign<A, B> = TODO;
+  type Assign<A, B> = Omit<A, keyof B> & B;
 
   const assign = <A, B>(obj1: A, obj2: B): Assign<A, B> => ({
     ...obj1,
