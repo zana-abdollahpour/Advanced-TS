2681a66f6b8ed5e3cc7a49ab31f47f4e18a6c0e6
zana-abdollahpour <zana.ap4@gmail.com>
2025-06-07 14:56:53 +0330

    4-5

--- src/4-Conditional-Types/5-challange.ts
+++ src/4-Conditional-Types/5-challange.ts
@@ -1,7 +1,9 @@
 import { Equal, Expect } from "..";
 
 namespace unwrapPromise {
-  type UnwrapPromise<Input> = TODO;
+  type UnwrapPromise<Input> = Input extends Promise<infer T>
+    ? T
+    : "NOT A PROMISE";
 
   type res1 = UnwrapPromise<Promise<"Hello">>;
   type test1 = Expect<Equal<res1, "Hello">>;
